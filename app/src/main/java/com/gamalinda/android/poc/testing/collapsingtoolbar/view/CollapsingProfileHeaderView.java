365d6cba8501db95b390d30c32219be34e3d2c9b
Ino Gamalinda <ino@gamalinda.com>
2016-04-21 02:25:22 +0800

    Add attribute setters and refactor
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/gamalinda/android/poc/testing/collapsingtoolbar/view/CollapsingProfileHeaderView.java
+++ app/src/main/java/com/gamalinda/android/poc/testing/collapsingtoolbar/view/CollapsingProfileHeaderView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,10 +12,13 @@ import com.gamalinda.android.poc.testing.collapsingtoolbar.R;
 
 public class CollapsingProfileHeaderView extends LinearLayout {
 
+    private int profileDrawable;
     private String profileName;
     private String subtitle;
     private String misc;
+    private int miscIcon;
 
+    private ImageView profileImage;
     private TextView profileNameTextView;
     private TextView subtitleTextView;
     private TextView miscTextView;
@@ -32,9 +36,11 @@ public class CollapsingProfileHeaderView extends LinearLayout {
                 0, 0);
 
         try {
+            profileDrawable = a.getResourceId(R.styleable.CollapsingProfileHeaderView_profileImage, 0);
             profileName = a.getString(R.styleable.CollapsingProfileHeaderView_profileName);
             subtitle = a.getString(R.styleable.CollapsingProfileHeaderView_profileSubtitle);
             misc = a.getString(R.styleable.CollapsingProfileHeaderView_profileMisc);
+            miscIcon = a.getResourceId(R.styleable.CollapsingProfileHeaderView_profileMiscIcon, 0);
         } finally {
             a.recycle();
         }
@@ -49,14 +55,17 @@ public class CollapsingProfileHeaderView extends LinearLayout {
     }
 
     private void loadViews() {
+        profileImage = (ImageView) this.findViewById(R.id.profileImage);
         profileNameTextView = (TextView) this.findViewById(R.id.profileName);
         subtitleTextView = (TextView) this.findViewById(R.id.profileSubtitle);
         miscTextView = (TextView) this.findViewById(R.id.profileMisc);
     }
 
     private void applyAttributes() {
+        profileImage.setImageResource(profileDrawable);
         profileNameTextView.setText(profileName);
         subtitleTextView.setText(subtitle);
         miscTextView.setText(misc);
+        miscTextView.setCompoundDrawablesWithIntrinsicBounds(miscIcon, 0, 0, 0);
     }
 }
