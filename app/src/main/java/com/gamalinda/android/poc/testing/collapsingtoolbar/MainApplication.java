032a7d2449c5e2ba6036a17bd414bd964644644a
Ino Gamalinda <ino@gamalinda.com>
2016-06-29 16:56:17 +0800

    Update name and icon
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/gamalinda/android/poc/testing/collapsingtoolbar/MainApplication.java
+++ app/src/main/java/com/gamalinda/android/poc/testing/collapsingtoolbar/MainApplication.java
@@ -2,5 +2,14 @@ package com.gamalinda.android.poc.testing.collapsingtoolbar;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
 }
