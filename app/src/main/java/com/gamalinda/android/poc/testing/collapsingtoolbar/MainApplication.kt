d2b0c26107d0845445a262cc618974cea2fd1a07
Ino Gamalinda <ino@gamalinda.com>
2019-05-28 14:15:53 +0800

    Convert MainApplication to Kotlin
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

---
+++ app/src/main/java/com/gamalinda/android/poc/testing/collapsingtoolbar/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.gamalinda.android.poc.testing.collapsingtoolbar
+
+import android.app.Application
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
